731ca1c93ccdaf94d05daefa1b6f8e781e50310a
katamartin <katasm@gmail.com>
2015-07-16 17:01:51 -0700

    Add password length validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,12 @@
 class User < ActiveRecord::Base
+  attr_reader :password
+
   validates :email, :session_token, :password_digest, presence: true
   validates :email, uniqueness: true
+  validates :password, length: { minimum: 6, allow_nil: true }
 
   after_initialize :ensure_session_token
+
 
   def self.generate_session_token
     SecureRandom.urlsafe_base64
