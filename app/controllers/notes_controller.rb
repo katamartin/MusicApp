906f9249f65e9fd0c4013b1e4ff65e953c0781c9
katamartin <katasm@gmail.com>
2015-07-16 17:42:45 -0700

    Integrate user with note functionality

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -5,6 +5,15 @@ class NotesController < ApplicationController
     redirect_to track_url(@note.track)
   end
 
+  def destroy
+    @note = Note.find(params[:id])
+    if @note
+      track = @note.track
+      @note.destroy
+      redirect_to track_url(track)
+    end
+  end
+
   def note_params
     params.require(:note).permit(:body, :track_id)
       .merge(user_id: current_user.id)
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   resources :bands
   resources :albums, except: :index
   resources :tracks, except: :index
-  resources :notes, only: :create
+  resources :notes, only: [:create, :destroy]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
